730bd6518a57f47e5749325d1923cd4bb67abaa1
Baillie O'Grady <baillieogrady@Baillies-iMac.local>
2020-05-20 09:54:44 +0100

    Remove duplicating button

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -15,12 +15,12 @@ class Intro extends React.Component {
             <div dangerouslySetInnerHTML={{ __html: text }} />
             : null
           }
-          {page === 'index' ?
+          {/* {page === 'index' ?
             <div className="mt4 mt0-ns">
               <Link to="/contact" className="white br2 tracked f7 fw7 ph3 pv2 no-underline  dim dib bg-green ttu">I'm available</Link>
             </div>
             : null
-          }
+          } */}
         </div>
       </section>
     )
