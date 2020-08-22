13f81d6612a6393a94d4931c90b7396ef08a32e3
Baillie O'Grady <baillieogrady@Baillies-iMac.local>
2020-08-22 20:27:25 +0100

    Add newsletter block to blog posts

--- src/templates/post.js
+++ src/templates/post.js
@@ -69,12 +69,12 @@ export default class PostTemplate extends Component {
           </div>
         </div>
         <TwoColumn
-          classes="bg-brand-primary-900 text-white"
-          lineClass="bg-white"
-          lead={{ text: "HIRE ME", class: "text-white" }}
-          heading={{ text: "Let's work together", class: "text-white" }}
-          text="<p>I’m currently available to convert your designs to a simple to use, intuitive, WordPress theme.</p>"
-          btn={{ url: "/convert-your-designs", text: "Convert your designs", classes: "bg-white text-brand-primary-800 hover:bg-gray-200" }}
+          classes="bg-brand-grey-200 relative z-10"
+          lineClass="bg-brand-primary-800"
+          lead={{ text: "JOIN", class: "text-black" }}
+          heading={{ text: "My weekly newsletter", class: "" }}
+          text="<p>Summarizing all my weekly coding content including live code examples, blogs, YouTube tutorials and Instagram carousels.</p>"
+          form="newsletter"
         />
       </Layout>
     )
